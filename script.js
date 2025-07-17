acd89098a75103160acdc34649193cb966353ff9
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-07-17 14:43:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('https://www.a7med-alshatebi.tech//api/instagram');
+        const response = await fetch('https://ins-ten.vercel.app/api/instagram');
         
         if (!response.ok) {
             throw new Error('Network response was not ok');
